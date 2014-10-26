34921f610a25285c59b7968b8efccf3f35f33dea
ShnaiderPavel <shnaiderpasha@gmail.com>
2014-10-27 00:29:56 +0400

    settings window running from menu

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 
 import sys
-from PySide.QtGui import QMainWindow, QPushButton, QApplication
+from PySide.QtGui import QMainWindow, QPushButton, QApplication, QDialog, QWidget
+from PySide.QtCore import QObject
 
 from ui_mainwindow import Ui_MainWindow
 from ui_settings import Ui_SettingsWindow
 
+class SettingsWindow(QWidget, Ui_SettingsWindow):
+	def __init__(self, parent=None):
+		super(SettingsWindow, self).__init__(parent)
+		self.ui = Ui_SettingsWindow()
+		self.setupUi(self)
+		
+		print "settings window ctor"
+
 class MainWindow(QMainWindow, Ui_MainWindow):
 	def __init__(self, parent=None):
 		super(MainWindow, self).__init__(parent)
+		self.ui = Ui_MainWindow()
 		self.setupUi(self)
 		
-		#self.SettingsAction.triggered.connect()
+		self.SettingsAction.triggered.connect(self.settings)
+		
+		self.set = SettingsWindow()
+		
+	def settings(self):
+		print "settings"
+		self.set.show()
 
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
