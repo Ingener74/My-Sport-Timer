1e9addf7ed994686152ae5fc18d3581565e9630d
ShnaiderPavel <shnaiderpasha@gmail.com>
2014-10-31 00:04:36 +0400

    sport timer work

--- main.py
+++ main.py
@@ -10,6 +10,35 @@ try:
 except Exception as e:
 	print e
 
+class SportTimer:
+	def __init__(self, minutes, seconds, onCountdown):
+		if minutes > 59 or minutes < 0:
+			raise RuntimeError("minutes must be less than 59 and more than 0")
+		self.min           = minutes
+		if seconds > 59 or seconds < 0:
+			raise RuntimeError("secons must be less than 59 and more than 0")
+		self.sec           = seconds
+		self._onCountdown  = onCountdown
+
+	def dec(self):
+		if self.sec == 0 and self.min == 0:
+			self._onCountdown()
+		elif self.sec == 0 and self.min != 0:
+			self.min -= 1
+			self.sec = 59
+		else:
+			self.sec -= 1
+	
+	def __str__(self):
+		outstr = ''
+		if self.min < 10:
+			outstr += '0'
+		outstr += str(self.min) + ':'
+		if self.sec < 10:
+			outstr += '0'
+		outstr += str(self.sec)
+		return outstr
+
 class SettingsWindow(QWidget, Ui_SettingsWindow):
 	def __init__(self, parent=None):
 		super(SettingsWindow, self).__init__(parent)
@@ -23,6 +52,31 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 		self.SettingsAction.triggered.connect(self.settings)
 		
 		self.set = SettingsWindow()
+		
+		self.newSportTimer()
+		self.lcdNumberTime.display(str(self._sportTimer))
+		
+		self.pushButtonStart.connect(SIGNAL('clicked()'), self.start_button)
+		
+	def OnCountdown(self):
+		self.newSportTimer()
+		QSound('data/bell.wav').play()
+#		self.TrayIcon.showMessage(u'Спортивный таймер...', u'Пора позаниматься!!!', qg.QSystemTrayIcon.Information, 2000)
+		
+	def newSportTimer(self):
+		self._sportTimer = SportTimer(self.set.spinBoxSessionMinutes.value() - 1, 59, self.OnCountdown)
+		
+	def start_button(self):
+		self.Timer1 = self.startTimer(1000)
+# 		if(self.Timer1.is_active()):
+# 			self.Timer1.stop()
+# 		else:
+# 			self.Timer1.start()
+		
+	def timerEvent(self, *args, **kwargs):
+		self._sportTimer.dec()
+		self.lcdNumberTime.display(str(self._sportTimer))
+		return QMainWindow.timerEvent(self, *args, **kwargs)
 		
 	def settings(self):
 		self.set.show()
