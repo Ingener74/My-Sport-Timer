bb2c56137fbd398cb9cd1ee7dd7ae17b64151c28
ShnaiderPavel <shnaiderpasha@gmail.com>
2014-11-10 21:27:08 +0400

    prepare fixed

--- prepare.py
+++ prepare.py
@@ -3,7 +3,7 @@
 
 import subprocess
 
-subprocess.Popen('pyside-rcc data/resouces.qrc  -o resouces_rc.py')
+subprocess.Popen(['pyside-rcc', 'data/resouces.qrc',  '-o', 'resouces_rc.py'])
 
-subprocess.Popen('pyside-uic data/mainwindow.ui -o ui_mainwindow.py')
-subprocess.Popen('pyside-uic data/settings.ui   -o ui_settings.py')
+subprocess.Popen(['pyside-uic', 'data/mainwindow.ui', '-o', 'ui_mainwindow.py'])
+subprocess.Popen(['pyside-uic', 'data/settings.ui'  , '-o', 'ui_settings.py'])
